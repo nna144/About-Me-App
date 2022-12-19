402fd3ba39d41c26c422e84c890bdf1879a4be35
Lina Vo <llina.tvo@gmail.com>
2022-12-19 21:31:49 +0100

    Initial Commit

---
+++ About Me App/About_Me_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  About_Me_AppApp.swift
+//  About Me App
+//
+//  Created by Lina Vo on 2022-12-19.
+//
+
+import SwiftUI
+
+@main
+struct About_Me_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
